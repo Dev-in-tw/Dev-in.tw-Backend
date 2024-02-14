1f65965e90ebb4adf5f22e625037813a473ac678
OnCloud <anonymousaaaa41414141@gmail.com>
2024-02-15 05:02:59 +0800

    Edit root route response
    +test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 
 
 describe("AppController", () => {
@@ -8,16 +7,17 @@ describe("AppController", () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService]
+      controllers: [AppController]
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe("root", () => {
-    it("should return \"Hello World!\"", () => {
-      expect(appController.getHello()).toBe("Hello World!");
+    it("should return an json object which contains welcome message", () => {
+      expect(appController.getWelcome()).toStrictEqual({
+        message: "Welcome to Dev-in.tw backend API"
+      });
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
 
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getWelcome(): { message: string } {
+    return {
+      message: "Welcome to Dev-in.tw backend API"
+    };
   }
 }
