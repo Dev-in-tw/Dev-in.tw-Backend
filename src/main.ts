bb37af5e5ce381f105966b09fa4cd8a3bea7a95e
OnCloud <anonymousaaaa41414141@gmail.com>
2024-02-26 11:40:03 +0800

    feat: :globe_with_meridians: edit swagger document path
    Shorten path "document" to "docs"

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     // .addTag("cats")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("document", app, document);
+  SwaggerModule.setup("docs", app, document);
 
   await app.listen(process.env.PORT || 3001);
 }
